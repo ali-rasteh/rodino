36c804c286bb2a351542f837f2c94539cd997eb5
saraghazanfari <s.ghazanfari.ce@gmail.com>
2023-12-12 22:04:47 -0500

    add attack to embedding

--- eval_knn.py
+++ eval_knn.py
@@ -115,44 +115,46 @@ def extract_features(model, data_loader, use_cuda=True, multiscale=False, is_tes
         samples = samples.cuda(non_blocking=True)
         if is_test:
             samples = generate_attack(img_ref=samples, target_model=model, eps=0.01)
-        index = index.cuda(non_blocking=True)
-        if multiscale:
-            feats = utils.multi_scale(samples, model)
-        else:
-            feats = model(samples).clone()
 
-        # init storage feature matrix
-        if dist.get_rank() == 0 and features is None:
-            features = torch.zeros(len(data_loader.dataset), feats.shape[-1])
-            if use_cuda:
-                features = features.cuda(non_blocking=True)
-            print(f"Storing features into tensor of shape {features.shape}")
-
-        # get indexes from all processes
-        y_all = torch.empty(dist.get_world_size(), index.size(0), dtype=index.dtype, device=index.device)
-        y_l = list(y_all.unbind(0))
-        y_all_reduce = torch.distributed.all_gather(y_l, index, async_op=True)
-        y_all_reduce.wait()
-        index_all = torch.cat(y_l)
-
-        # share features between processes
-        feats_all = torch.empty(
-            dist.get_world_size(),
-            feats.size(0),
-            feats.size(1),
-            dtype=feats.dtype,
-            device=feats.device,
-        )
-        output_l = list(feats_all.unbind(0))
-        output_all_reduce = torch.distributed.all_gather(output_l, feats, async_op=True)
-        output_all_reduce.wait()
-
-        # update storage feature matrix
-        if dist.get_rank() == 0:
-            if use_cuda:
-                features.index_copy_(0, index_all, torch.cat(output_l))
+        with torch.no_grad():
+            index = index.cuda(non_blocking=True)
+            if multiscale:
+                feats = utils.multi_scale(samples, model)
             else:
-                features.index_copy_(0, index_all.cpu(), torch.cat(output_l).cpu())
+                feats = model(samples).clone()
+
+            # init storage feature matrix
+            if dist.get_rank() == 0 and features is None:
+                features = torch.zeros(len(data_loader.dataset), feats.shape[-1])
+                if use_cuda:
+                    features = features.cuda(non_blocking=True)
+                print(f"Storing features into tensor of shape {features.shape}")
+
+            # get indexes from all processes
+            y_all = torch.empty(dist.get_world_size(), index.size(0), dtype=index.dtype, device=index.device)
+            y_l = list(y_all.unbind(0))
+            y_all_reduce = torch.distributed.all_gather(y_l, index, async_op=True)
+            y_all_reduce.wait()
+            index_all = torch.cat(y_l)
+
+            # share features between processes
+            feats_all = torch.empty(
+                dist.get_world_size(),
+                feats.size(0),
+                feats.size(1),
+                dtype=feats.dtype,
+                device=feats.device,
+            )
+            output_l = list(feats_all.unbind(0))
+            output_all_reduce = torch.distributed.all_gather(output_l, feats, async_op=True)
+            output_all_reduce.wait()
+
+            # update storage feature matrix
+            if dist.get_rank() == 0:
+                if use_cuda:
+                    features.index_copy_(0, index_all, torch.cat(output_l))
+                else:
+                    features.index_copy_(0, index_all.cpu(), torch.cat(output_l).cpu())
     return features
 
 
